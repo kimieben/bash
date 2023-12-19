b8853b1ae71833c51810d814678380ce03e37c7c
kimie <kimieben2000@gmail.com>
2023-12-19 09:15:17 +0800

    why not working

---
+++ python/timing.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+
+start_time = datetime.now()
+print(f"Start time: {datetime.now().strftime('%H')}")
+
+interval = 100
+
+while interval != 0:
+    if (datetime.now() - start_time).seconds == 1:
+        start_time = datetime..now()
+        print(f"Current time: {datetime.now().strftime('%H')}")
+        interval -= 1
+
