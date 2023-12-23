47a0e70c54d81371b0bdad1a84346d5bfaee5a19
kimie <kimieben2000@gmail.com>
2023-12-23 11:44:51 +0800

    hm

--- python/timing.py
+++ python/timing.py
@@ -3,13 +3,12 @@
 from datetime import datetime
 
 start_time = datetime.now()
-print(f"Start time: {datetime.now().strftime('%H')}")
+print(f"Start time: {datetime.now().strftime('%H:%M:%S')}")
 
-interval = 100
+interval = 10
 
 while interval != 0:
     if (datetime.now() - start_time).seconds == 1:
-        start_time = datetime..now()
-        print(f"Current time: {datetime.now().strftime('%H')}")
+        start_time = datetime.now()
+        print(f"Current time: {datetime.now().strftime('%H:%M:%S')}")
         interval -= 1
-
